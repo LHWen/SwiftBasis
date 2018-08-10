fa4a78da570d5058ed62f3eac8e45496926c2758
LHWen <1109070804@qq.com>
2018-08-10 09:45:43 +0800

    set lable layer radius and make action : #selector(func)

--- SwiftBasis/LableViewController.swift
+++ SwiftBasis/LableViewController.swift
@@ -16,10 +16,24 @@ class LableViewController: UIViewController {
         self.navigationItem.title = "UILable"
         view.backgroundColor = UIColor.white
         
-        let helloFrame = CGRect.init(x: 13.0, y: 40.0, width: 100, height: 20)
+        let helloFrame = CGRect.init(x: 13.0, y: 40.0, width: 160, height: 30)
         let helloLbl = setLableInit(textStr: "Hello", frame: helloFrame, tColor: UIColor.black, bColor: UIColor.orange, textA: NSTextAlignment.center, fSize: 16.0)
         view.addSubview(helloLbl)
         
+        let rFrame = CGRect.init(x: 13.0, y: 80.0, width: 160, height: 30)
+        let rLable = setRadiusLable(tStr: "text String", frame: rFrame, tColor: UIColor.black, bColor: UIColor.orange, textA: NSTextAlignment.center, fSize: 16.0, radius: 4.0)
+        view.addSubview(rLable)
+        
+        let cFrame = CGRect.init(x: 13.0, y: 130.0, width: 160.0, height: 40.0)
+        let cLable = setLableInit(textStr: "click Lable", frame: cFrame, tColor: UIColor.black, bColor: UIColor.purple, textA: NSTextAlignment.center, fSize: 16.0)
+        cLable.isUserInteractionEnabled = true
+        cLable.addGestureRecognizer(UITapGestureRecognizer.init(target: self, action: #selector(testLableClick)))
+        view.addSubview(cLable)
+        
+    }
+    
+    @objc func testLableClick() {
+        print("click Lable is success!")
     }
     
     func setLableInit(textStr: String, frame: CGRect, tColor: UIColor, bColor: UIColor, textA:NSTextAlignment, fSize: CGFloat) -> UILabel {
@@ -32,6 +46,19 @@ class LableViewController: UIViewController {
         label.text = textStr
         
         return label
+    }
+    
+    func setRadiusLable(tStr: String, frame: CGRect, tColor: UIColor, bColor: UIColor, textA: NSTextAlignment, fSize: CGFloat, radius: CGFloat) -> UILabel {
+        
+        let lable = UILabel.init(frame: frame)
+        lable.text = tStr
+        lable.backgroundColor = bColor
+        lable.textColor = tColor
+        lable.textAlignment = textA
+        lable.layer.cornerRadius = radius
+        lable.layer.masksToBounds = true
+        
+        return lable
     }
 
     override func didReceiveMemoryWarning() {
